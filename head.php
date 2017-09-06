908f81d8cfd53003ea1f27a9ef6199630790ec1d
wangchenchen <wangchenchen@ffrj.net>
2017-09-06 13:19:50 +0800

    * change:优化;

--- head.php
+++ head.php
@@ -4,18 +4,13 @@
     <meta name="description" content="中国十大最美乡村">
     <meta name="author" content="zdy">
     <title>美丽乡村</title>
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/animate.min.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
     <link href="css/responsive.css" rel="stylesheet">
     <link href="css/barrager.css" rel="stylesheet">
     <link href="css/common.css" rel="stylesheet">
-
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
 
     <script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/jquery.barrager.js"></script>
